e8e05a3e27fdd27d19364a86812679e4304e12fa
evellyn severo <evellyn.severo@zup.com.br>
2026-04-14 14:46:31 -0300

    test: ajusta teste de contexto em AppUsuarioApplicationTests

---
+++ src/test/java/com/example/appusuario/AppusuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppusuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
