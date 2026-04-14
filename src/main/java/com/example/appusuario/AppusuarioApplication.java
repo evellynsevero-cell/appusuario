57ed5a95c5cfd585ca6d82d0a49800d1635df6ef
evellyn severo <evellyn.severo@zup.com.br>
2026-04-14 14:42:02 -0300

    feat: adiciona classe principal AppUsuarioApplication

---
+++ src/main/java/com/example/appusuario/AppusuarioApplication.java
@@ -0,0 +1,13 @@
+package com.example.appusuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppusuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppusuarioApplication.class, args);
+	}
+
+}
