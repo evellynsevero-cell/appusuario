8e784c4ca3304701453ec9347a93a008a32fd170
evellyn severo <evellyn.severo@zup.com.br>
2026-04-14 15:02:54 -0300

    feat: ajusta campos e estrutura do UsuarioResponse

---
+++ src/main/java/com/example/appusuario/UsuarioResponse.java
@@ -0,0 +1,17 @@
+package com.example.appusuario;
+
+public class UsuarioResponse {
+    private Long id;
+    private String nome;
+    private String email;
+
+    public UsuarioResponse(Long id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Long getId() { return id; }
+    public String getNome() { return nome; }
+    public String getEmail() { return email; }
+}
